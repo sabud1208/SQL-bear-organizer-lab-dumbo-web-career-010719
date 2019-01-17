131bcafae434d29c8ac747d6452a2964a45ddeac
Stephanie Abud <sabud1208@gmail.com>
2019-01-17 15:19:32 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,8 +1,8 @@
-INSERT INTO bears ("Sally", 4, "Female", "Brown", 10, "Alive");
-INSERT INTO bears ("Melissa", 4, "Female", "Brown", 9, "Alive");
-INSERT INTO bears ("Britney", 5, "Female", "Caramel", 7, "Alive");
-INSERT INTO bears ("Tom" 5, "Male", "Chocolate", 7, "Alive");
-INSERT INTO bears ("Brady", 7, "Male", "Chocolate", 8, "Alive");
-INSERT INTO bears ("Marshall", 2, "Male" "Brown", 4, "Alive");
-INSERT INTO bears ("Mathers", 9, "Male", "Black", 4, "Alive");
+INSERT INTO bears (name, age, gender, color, temperament, alive) values ("Sally", 4, "Female", "Brown", 10, "Alive");
+INSERT INTO bears (name, age, gender, color, temperament, alive) values ("Melissa", 4, "Female", "Brown", 9, "Alive");
+INSERT INTO bears  (name, age, gender, color, temperament, alive) values ("Britney", 5, "Female", "Caramel", 7, "Alive");
+INSERT INTO bears  (name, age, gender, color, temperament, alive) values ("Tom" 5, "Male", "Chocolate", 7, "Alive");
+INSERT INTO bears  (name, age, gender, color, temperament, alive) values  ("Brady", 7, "Male", "Chocolate", 8, "Alive");
+INSERT INTO bears  (name, age, gender, color, temperament, alive) values ("Marshall", 2, "Male" "Brown", 4, "Alive");
+INSERT INTO bears (name, age, gender, color, tem("Mathers", 9, "Male", "Black", 4, "Alive");
 INSERT INTO bears ("Cassie", 4, "Female", "Brown", 4, "Alive");
