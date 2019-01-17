461efb09883dd3d8ded63346a79ce22577dd6e10
Stephanie Abud <sabud1208@gmail.com>
2019-01-17 15:08:51 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,1 +1,3 @@
 INSERT INTO bears ("Sally", 4, "Female", "Brown", 10, "Alive");
+INSERT INTO bears ("Sally", 4, "Female", "Brown", 9, "Alive");
+INSERT INTO bears ("Britney", 5, "Female", "Caramel", 7, "Alive")
